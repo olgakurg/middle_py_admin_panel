68e027e5fecf7a0d310dbe658734d0bd7fa16626
olgakurg <olgakurg@gmail.com>
2023-09-09 11:44:27 +0800

    add models, update admin

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -1,8 +1,28 @@
 
 from django.contrib import admin
-from .models import Genre
+from .models import Genre, Filmwork, Person, GenreFilmwork, PersonFilmwork
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
     pass  
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    pass  
+
+class GenreFilmworkInline(admin.TabularInline):
+    model = GenreFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmworkInline,) 
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
+
+
+@admin.register(Filmwork)
+class FilmworkAdmin(admin.ModelAdmin):
+    inlines = (PersonFilmworkInline,) 
--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,19 +1,27 @@
 import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
-
-class Genre(models.Model):
-    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
-    # Вам же придётся явно объявить primary key.
+class UUIDMixin(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+class TimeStampedMixin(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        
+        abstract = True
+        
+class Genre(UUIDMixin, TimeStampedMixin):
     # Первым аргументом обычно идёт человекочитаемое название поля
     name = models.CharField('name', max_length=255)
     # blank=True делает поле необязательным для заполнения.
     description = models.TextField('description', blank=True)
-    # auto_now_add автоматически выставит дату создания записи 
-    created = models.DateTimeField(auto_now_add=True)
-    # auto_now изменятся при каждом обновлении записи 
-    modified = models.DateTimeField(auto_now=True)
 
     class Meta:
         # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
@@ -24,24 +32,68 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
-
-class Filmwork(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+class Person(UUIDMixin, TimeStampedMixin):
     # Первым аргументом обычно идёт человекочитаемое название поля
-    title = models.CharField('name', max_length=255)
-    # blank=True делает поле необязательным для заполнения.
-    description = models.TextField('description', blank=True)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    created = models.DateTimeField(auto_now_add=True)
-    # auto_now изменятся при каждом обновлении записи
-    modified = models.DateTimeField(auto_now=True)
-
+    full_name = models.TextField('full_name')
     class Meta:
         # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
-        db_table = "content\".\"genre"
+        db_table = "content\".\"person"
         # Следующие два поля отвечают за название модели в интерфейсе
-        verbose_name = 'Жанр'
-        verbose_name_plural = 'Жанры'
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники' 
+
+    def __str__(self):
+        return self.full_name
+
+class Filmwork(UUIDMixin, TimeStampedMixin):
+  
+    title = models.CharField('name', max_length=255)
+    description = models.TextField('description', blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    
+    class TypeOfFilm(models.TextChoices):
+        MOVIE = 'MV', _('Movie')
+        TV_SHOW = 'SH', _('TV Show')
+
+    type_of_film = models.CharField(
+        max_length=2,
+        choices=TypeOfFilm.choices,
+        default=TypeOfFilm.MOVIE,
+    )
+
+    def is_upperclass(self):
+        return self.type_of_film in {
+            self.type_of_film.MOVIE,
+            self.type_of_film.TV_SHOW,
+        } 
+            
+    rating = models.FloatField('rating', blank=True,
+                               validators=[MinValueValidator(0),
+                                           MaxValueValidator(100)]) 
+    
+    genres = models.ManyToManyField(Genre, through='GenreFilmwork')
+    
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content\".\"film_work"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
 
     def __str__(self):
         return self.title 
+
+class GenreFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    genre = models.ForeignKey('Genre', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "content\".\"genre_film_work" 
+
+class PersonFilmwork(UUIDMixin):
+    film_work = models.ForeignKey('Filmwork', on_delete=models.CASCADE)
+    person = models.ForeignKey('Person', on_delete=models.CASCADE)
+    role = models.TextField('role', null=True)
+    created = models.DateTimeField(auto_now_add=True) 
