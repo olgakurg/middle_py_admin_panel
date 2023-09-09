9f74d3040948b4f1f9d2e207840eec04ec3c4570
olgakurg <olgakurg@gmail.com>
2023-09-09 14:37:38 +0800

    Update admin.py

--- movies_admin/movies/admin.py
+++ movies_admin/movies/admin.py
@@ -7,16 +7,21 @@ from .models import Genre, Filmwork, Person, GenreFilmwork, PersonFilmwork
 class GenreAdmin(admin.ModelAdmin):
     pass  
 
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass  
+
 class GenreFilmworkInline(admin.TabularInline):
     model = GenreFilmwork
+
+class PersonFilmworkInline(admin.TabularInline):
+    model = PersonFilmwork
 
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline,)
-
-class PersonFilmworkInline(admin.TabularInline):
-    model = PersonFilmwork
     # Отображение полей в списке
     list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
     list_filter = ('type',)
     search_fields = ('title', 'description', 'id')
+
